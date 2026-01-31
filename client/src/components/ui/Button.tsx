ad396235db831155bf3a6870f163ebd9c5ffd7e1
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2026-01-31 13:37:26 -0500

    next button styling

--- client/src/components/ui/Button.tsx
+++ client/src/components/ui/Button.tsx
@@ -1,6 +1,8 @@
+import '../../styles/globals.css';
+
 export const Button = ({ onClick, children }: { onClick: () => void, children: React.ReactNode }) => {
     return (
-        <button onClick={onClick}>
+        <button onClick={onClick} className="button">
             {children}
         </button>
     );
