92e4f49cddc463115c8e430df644f69e639dff6b
Khadeeja H <147099250+theRedKh@users.noreply.github.com>
2026-01-31 20:55:44 -0500

    fix router

--- client/src/app/router.tsx
+++ client/src/app/router.tsx
@@ -6,13 +6,11 @@ import BasePage from "../pages/BasePage";
 import BattlePage from "../pages/BattlePage";
 import EndPage from "../pages/EndPage";
 import DeathPage from "../pages/DeathPage";
-import InfoCardsPage from "../pages/InfoCardsPage";
 
 export const router = createBrowserRouter([
   { path: '/', element: <PreIntroPage /> },
   { path: '/intro', element: <IntroPage /> },
   { path: '/bank', element: <BasePage /> },
-  { path: '/info', element: <InfoCardsPage /> },
   { path: '/map', element: <MapPage /> },
   { path: '/battle', element: <BattlePage /> },
   { path: '/end', element: <EndPage /> },
--- client/src/pages/InfoCardsPage.tsx
+++
@@ -1,8 +0,0 @@
-export default function InfoCardsPage() {
-    return (
-        <main>
-            <h1>Info</h1>
-            <p>Info Cards Page</p>
-        </main>
-    );
-}
